8c8f304d5c19cb8f4c518679b7bfa7a1d11d4e97
DanielNiven <2481553@dundee.ac.uk>
2023-02-02 16:00:18 +0000

    Added functionality to menu

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -5,11 +5,47 @@ namespace Game
         public Form1()
         {
             InitializeComponent();
+            lbl_name.Hide();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            textBox1.Text = "Hello world";
+        }
+
+        private void btn_Quit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_username_TextChanged(object sender, EventArgs e)
+        {
+            lbl_name.Text = "You are signed in as: " + txt_username.Text;
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            Form game = new GameBoard();
+            game.Show();
+            Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lbl_name.Show();
+            txt_username.Hide();
+            btn_Apply.Hide();
+        }
+
+        private void btn_Tut_Click(object sender, EventArgs e)
+        {
+            Form T = new HowToPlay();
+            T.Show();
+            Hide();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
