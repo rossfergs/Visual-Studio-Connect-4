18126d3b25016c5dd15548e803a1722adf8b51f5
rossfergs <91530725+rossfergs@users.noreply.github.com>
2023-02-07 16:32:04 +0000

    Add files via upload

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -1,13 +1,18 @@
+using System;
+
 namespace Connect4
 {
     public partial class Form1 : Form
     {
 
         Button[,] btn = new Button[8,7];
-            
+        Color player = Color.Red;
+
         public Form1()
         {
             InitializeComponent();
+
+            char text = ' ';
 
             for (int i = 1; i < 8; i++)
             {
@@ -16,8 +21,8 @@ namespace Connect4
                     btn[i, j] = new Button();
                     btn[i, j].SetBounds(40 + (40 * i), 40 + (40 * j), 35, 35);
                     btn[i, j].BackColor = Color.DimGray;
-                    btn[i, j].Text = Convert.ToString(' ');
-                    btn[i, j].Click += new EventHandler(this.btnEvent_Handler);
+                    btn[i, j].Text = Convert.ToString(text);
+                    btn[i, j].Click += new EventHandler(btnEvent_Handler);
                     Controls.Add(btn[i, j]);
                 }
             }
@@ -25,22 +30,48 @@ namespace Connect4
 
         void btnEvent_Handler(object sender, EventArgs e)
         {
-            ((Button)sender).BackColor = Color.Blue;
-
-            placePiece()
+            placePiece(btn, ((Button)sender), player);
         }
 
-        void placePiece(int input, Button[,] board, Color player)
+        void placePiece(Button[,] btn, Button input, Color player)
         {
-
-            int counter = 1;
-
-            while (board[input, counter].BackColor == Color.DimGray)
+            if(input.BackColor == Color.DimGray)
             {
-                counter += 1;
+                input.BackColor = player;
             }
 
-            board[input, counter].BackColor = player;
+            movePieces(btn);
+
+            if(player == Color.Red)
+            {
+                player = Color.Yellow;
+            }
+            else
+            {
+                player = Color.Red;
+            }
+
+        }
+
+        void movePieces(Button[,] btn)
+        {
+
+            int width = btn.GetLength(0);
+            int height = btn.GetLength(1);
+
+
+            for (int i = 1; i < width; i++)
+            {
+                for (int j = 1; j < height; j++)
+                {
+                    if (btn[i, j].BackColor != Color.DimGray && btn[i+1, j+1].BackColor == Color.DimGray)
+                    {
+                        btn[i + 1, j + 1].BackColor = btn[i, j].BackColor;
+                        btn[i, j].BackColor = Color.DimGray;
+                        
+                    }
+                }
+            }
 
         }
     }
